10ed91e0ccde8bc712652706c73a5893b13051f1
Jiangge Zhang <tonyseek@gmail.com>
2014-04-07 16:28:30 +0800

    implement the item object.

--- flask_navigation/item.py
+++ flask_navigation/item.py
@@ -1,5 +1,52 @@
 import collections
 
+from flask import url_for
+
+
+class Item(object):
+    """The navigation item object.
+
+    :param label: the display label of this navigation item.
+    :param endpoint: the unique name of this navigation item.
+                     If this item point to a internal url, this parameter
+                     should be acceptable for ``url_for`` which will generate
+                     the target url.
+    :param args: optional. If this parameter be provided, it will be passed to
+                 the ``url_for`` with ``endpoint`` together.
+                 Maybe this arguments need to be decided in the Flask app
+                 context, then this parameter could be a function to delay the
+                 execution.
+    :param url: optional. If this parameter be provided, the target url of
+                this navigation will be it. The ``endpoint`` and ``args`` will
+                not been used to generate url.
+
+    The ``endpoint`` is the identity name of this navigation item. It will be
+    unique in whole application. In mostly situation, it should be a endpoint
+    name of a Flask view function.
+    """
+
+    def __init__(self, label, endpoint, args=None, url=None):
+        self.label = label
+        self.endpoint = endpoint
+        self._args = args
+        self._url = url
+
+    @property
+    def args(self):
+        """The arguments which will be passed to ``url_for``."""
+        if self._args is None:
+            return {}
+        if callable(self._args):
+            return dict(self._args())
+        return dict(self._args)
+
+    @property
+    def url(self):
+        """The final url of this navigation item."""
+        if self._url is None:
+            return url_for(self.endpoint, **self.args)
+        return self._url
+
 
 class ItemCollection(collections.MutableSequence):
     """The collection of navigation items.
