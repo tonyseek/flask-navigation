3dab827a46a2a41e151506792ce3b660b9d5d99c
Jiangge Zhang <tonyseek@gmail.com>
2014-04-13 16:14:01 +0800

    add bound type property.

--- flask_navigation/utils.py
+++ flask_navigation/utils.py
@@ -8,9 +8,45 @@ def freeze_dict(dict_):
 
     e.g.::
 
-    >>> freeze_dict({'a': 1, 'b': 2})
-    (('a', 1), ('b', 2))
+        >>> freeze_dict({'a': 1, 'b': 2})
+        (('a', 1), ('b', 2))
     """
     pairs = dict_.items()
     key_getter = operator.itemgetter(0)
     return tuple(sorted(pairs, key=key_getter))
+
+
+class BoundTypeProperty(object):
+    """This kind of property creates subclasses of given class for each
+    instance.
+
+    Those subclasses means "bound type" which be used for identifying
+    themselves with blinker/Flask signals.
+
+    e.g.::
+
+        >>> class Foo(object):
+        ...     pass
+        >>> class Bar(object):
+        ...     Foo = BoundTypeProperty('Foo', Foo)
+        >>> bar = Bar()
+        >>> bar.Foo is Foo
+        False
+        >>> issubclass(bar.Foo, Foo)
+        True
+
+    :param name: the name of this property.
+    :param cls: the base class of all bound classes.
+    """
+
+    def __init__(self, name, cls):
+        self.name = name
+        self.cls = cls
+
+    def __get__(self, instance, owner):
+        if instance is None:
+            return self
+        ns = vars(instance)  # the instance namespace
+        if self.name not in ns:
+            ns[self.name] = type(self.name, (self.cls,), {})
+        return ns[self.name]
